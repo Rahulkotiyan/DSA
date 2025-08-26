a834aeb7b117ab80c9959f1eb26d7e692e59b4f7
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2025-08-26 20:36:59 +0530

    strings1 done

--- strings/prog1.cpp
+++ strings/prog1.cpp
@@ -1,2 +1,30 @@
 //program to remove the outermost parenthesis from a string
 
+#include<bits/stdc++.h>
+using namespace std;
+
+string removePar(string s)
+{
+    int counter =0;
+    string ans ="";
+
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]==')') 
+            counter--;
+        if(counter!=0)
+            ans.push_back(s[i]);
+        if(s[i]=='(') 
+            counter++;
+    }
+    return ans;
+}
+int main()
+{
+    string s;
+    cout<<"Enter the string"<<endl;
+    cin>>s;
+    string ans =removePar(s);
+    cout<<ans;
+    return 0;
+}
